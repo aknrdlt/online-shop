01e115e425ca2121ae9e7b96238e5d0e503bf0d8
aknrdlt <aknrdlt@gmail.com>
2023-06-13 00:43:07 +0600

    add: Product model

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'slug',
+        'weight',
+        'width',
+        'height',
+        'price'
+    ];
+}
